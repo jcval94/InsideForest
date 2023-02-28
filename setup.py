0d1daf20a65370dede4d7389c8f7c39eaaab8daa
Jose Carlos <valc941226@gmail.com>
2023-02-28 00:11:31 -0600

    Cmbio Nombre

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Jose Carlos Del Valle',
     author_email='jcval94@gmail.com',
     description='A comprehensive library for describing and analyzing data insights via AI',
-    long_description='Pyctogram provides a comprehensive AI solution for identifying and describing categories within your data, allowing you to gain valuable insights and make data-driven decisions.',
+    long_description='InsideForest provides a comprehensive AI solution for identifying and describing categories within your data, allowing you to gain valuable insights and make data-driven decisions.',
     long_description_content_type='text/markdown',
     url='https://github.com/jcval94/InsideForest.git',
     
