1f8b5fa812cb1bc336f8398ee0d62ef02dce233f
Jose Carlos <valc941226@gmail.com>
2023-02-28 11:54:34 -0600

    posición init

---
+++ InsideForest/__init__.py
@@ -0,0 +1,4 @@
+from .labels import labels
+from .regions import regions
+from .trees import trees
+from .models import models
--- __init__.py
+++
@@ -1,4 +0,0 @@
-from InsideForest.labels import labels
-from InsideForest.regions import regions
-from InsideForest.trees import trees
-from InsideForest.models import models
