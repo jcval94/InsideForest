e03d61c31a704ea4151699ae280dd7430b47e77b
Jose Carlos <valc941226@gmail.com>
2023-02-28 11:14:10 -0600

    coautoria

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='InsideForest',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     license='MIT',
-    author='Jose Carlos Del Valle',
-    author_email='jcval94@gmail.com',
+    author=[('Jose Carlos Del Valle', 'jcval94@gmail.com'),
+            ('ChatGPT', 'chat.openai.com/chat')],
     description='A comprehensive library for describing and analyzing data insights via AI',
     long_description='InsideForest provides a comprehensive AI solution for identifying and describing categories within your data, allowing you to gain valuable insights and make data-driven decisions.',
     long_description_content_type='text/markdown',
