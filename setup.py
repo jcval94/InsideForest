a7d75716696b71b46d7ccd37c369552a7f856f34
Jose Carlos <valc941226@gmail.com>
2023-04-03 12:33:45 -0600

    Corrección de árboles que no aparecían

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='InsideForest',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     license='MIT',
     author=[('Jose Carlos Del Valle', 'jcval94@gmail.com'),
