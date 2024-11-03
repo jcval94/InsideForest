958df8761641fdfd1bb4335395d19e1f64be824c
José Carlos Del Valle <jcval94@gmail.com>
2024-11-02 23:41:26 -0600

    copy
    import copy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='InsideForest',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     license='MIT',
     author=[('Jose Carlos Del Valle', 'jcval94@gmail.com'),
