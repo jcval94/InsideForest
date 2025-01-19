c5922d6697169458ecb73e902c99b3e4419304b7
José Carlos Del Valle <jcval94@gmail.com>
2025-01-18 20:38:16 -0600

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='InsideForest',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     license='MIT',
     author=[('Jose Carlos Del Valle', 'jcval94@gmail.com'),
