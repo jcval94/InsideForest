1deb1b71a5ae912766b660bad5f7bad81414ffaa
José Carlos Del Valle <jcval94@gmail.com>
2025-01-21 09:01:54 -0600

    Actualizar a 2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='InsideForest',
-    version='0.2.3',
+    version='0.2.4',
     packages=find_packages(),
     license='MIT',
     author=[('Jose Carlos Del Valle', 'jcval94@gmail.com'),
