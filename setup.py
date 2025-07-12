582b1ab8c592fe73789f2262c4a6e87d9fb0dc1d
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2025-07-11 20:08:02 -0600

    Add MIT license file and reference in setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.2.14',
     packages=find_packages(),
     license='MIT',
+    license_files=['LICENSE'],
     author='Jose Carlos Del Valle',
     description='A comprehensive library for describing and analyzing data insights via AI',
     long_description='InsideForest provides a comprehensive AI solution for identifying and describing categories within your data, allowing you to gain valuable insights and make data-driven decisions.',
