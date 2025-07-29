b22f2d0ddd25241bc753a32d9b0cb39250d1d758
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2025-07-28 20:06:33 -0600

    Subir de versión

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='InsideForest',
-    version='0.2.17',
+    version='0.2.18',
     packages=find_packages(),
     license='MIT',
     license_files=['LICENSE'],
